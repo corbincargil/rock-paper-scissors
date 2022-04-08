77a96124d438e5d39579bed51fa86fb0362f4f5d
corbincargil <corbin.cargil@gmail.com>
2022-04-08 14:49:54 -0500

    completed live scoreboard. added background pictures to buttons. added live 'results' window.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -10,6 +10,9 @@
 let computerScore = 0; 
 let playerScore = 0;
 let playerChoice = 'scissors';
+let compPlayResult; 
+let roundResult;
+let compPlayOutput;
 
 //Random number (1-3) to be used for the computers selection
 let random = function() {
@@ -22,12 +25,15 @@ let computerPlay = function() {
     let option = random();
     if (option === 1) {
         console.log('Computer chooses: ' + 'rock');
+        compPlayResult = 'Rock '
         return 'rock';
     } else if (option === 2 ) {
         console.log('Computer chooses: ' + 'paper');
+        compPlayResult = 'Paper '
         return 'paper';
     } else if (option === 3) {
         console.log('Computer chooses: ' + 'scissors');
+        compPlayResult = 'Scissors '
         return 'scissors';
     } else {
         console.log('Invalid computer input.');
@@ -42,7 +48,8 @@ let singleRoundRock = function(playerSelection, computerSelection) {
 
     //Input from computer
     playerSelection = 'rock';   
-    console.log('Player chooses: '+playerSelection);computerSelection = computerPlay();
+    console.log('Player chooses: '+playerSelection);
+    computerSelection = computerPlay();
     
     //Conditional statements for each outcome
     
@@ -50,19 +57,44 @@ let singleRoundRock = function(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         console.log('This round was a tie!');
         console.log('The score is... Player: ' + playerScore + ' and Computer: ' + computerScore);
+        roundResult = 'This round was a draw.';
     //human loses
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
         console.log('The computer wins this round!');
+        roundResult = 'Computers win this round!';
         computerScore++;
         console.log('The score is... Player: ' + playerScore + ' and Computer: ' + computerScore);
     //player wins
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         console.log('Player 1 wins this round!');
+        roundResult = 'Humans win this round!';
         playerScore++;
         console.log('The score is... Player: ' + playerScore + ' and Computer: ' + computerScore);
     } else {
         console.log('An error occured this round.');
     }
+
+    //Update the human score on the score board
+    let humanScoreNumber = document.getElementById('human-score-number');
+    humanScoreNumber.textContent = playerScore;
+
+    //Update the computer score on the score board
+    let computerScoreNumber = document.getElementById('computer-score-number');
+    computerScoreNumber.textContent = computerScore;
+
+    //Remove prev results (if they exist) and output new results
+    if (compPlayOutput != null) {
+       let needRemoved = document.getElementById('computer-chose');
+        needRemoved.remove(); 
+    }
+    compPlayOutput = document.createElement('p')
+    compPlayOutput.setAttribute('id','computer-chose');
+    compPlayOutput.textContent = compPlayResult;
+    let compChoseDiv = document.getElementById('comp-chose-div');
+    compChoseDiv.appendChild(compPlayOutput);
+    
+    let roundResultsOutput = document.getElementById('round-result')
+    roundResultsOutput.textContent = roundResult;
 };
 
 //single round if player clicks paper button
@@ -70,7 +102,8 @@ let singleRoundPaper = function(playerSelection, computerSelection) {
 
     //Input from computer
     playerSelection = 'paper';   
-    console.log('Player chooses: '+playerSelection);computerSelection = computerPlay();
+    console.log('Player chooses: '+playerSelection);
+    computerSelection = computerPlay();
     
     //Conditional statements for each outcome
     
@@ -78,19 +111,45 @@ let singleRoundPaper = function(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         console.log('This round was a tie!');
         console.log('The score is... Player: ' + playerScore + ' and Computer: ' + computerScore);
+        roundResult = 'This round was a draw.';
     //human loses
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
         console.log('The computer wins this round!');
+        roundResult = 'Computers win this round!';
         computerScore++;
         console.log('The score is... Player: ' + playerScore + ' and Computer: ' + computerScore);
     //player wins
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         console.log('Player 1 wins this round!');
+        roundResult = 'Humans win this round!';
         playerScore++;
         console.log('The score is... Player: ' + playerScore + ' and Computer: ' + computerScore);
     } else {
         console.log('An error occured this round.');
     }
+
+    //Update the human score on the score board
+    let humanScoreNumber = document.getElementById('human-score-number');
+    humanScoreNumber.textContent = playerScore;
+
+    //Update the computer score on the score board
+    let computerScoreNumber = document.getElementById('computer-score-number');
+    computerScoreNumber.textContent = computerScore;
+
+
+    //Remove prev results (if they exist) and output new results
+    if (compPlayOutput != null) {
+        let needRemoved = document.getElementById('computer-chose');
+         needRemoved.remove(); 
+     }
+    compPlayOutput = document.createElement('p')
+    compPlayOutput.setAttribute('id','computer-chose');
+    compPlayOutput.textContent = compPlayResult;
+    let compChoseDiv = document.getElementById('comp-chose-div');
+    compChoseDiv.appendChild(compPlayOutput);
+    
+    let roundResultsOutput = document.getElementById('round-result')
+    roundResultsOutput.textContent = roundResult;
 };
 
 //single round if player clicks scissors button
@@ -98,7 +157,8 @@ let singleRoundScissors = function(playerSelection, computerSelection) {
 
     //Input from computer
     playerSelection = 'scissors';   
-    console.log('Player chooses: '+playerSelection);computerSelection = computerPlay();
+    console.log('Player chooses: '+playerSelection);
+    computerSelection = computerPlay();
     
     //Conditional statements for each outcome
     
@@ -106,23 +166,59 @@ let singleRoundScissors = function(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         console.log('This round was a tie!');
         console.log('The score is... Player: ' + playerScore + ' and Computer: ' + computerScore);
+        roundResult = 'This round was a draw.';
     //human loses
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         console.log('The computer wins this round!');
+        roundResult = 'Computers win this round!';
         computerScore++;
         console.log('The score is... Player: ' + playerScore + ' and Computer: ' + computerScore);
     //player wins
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         console.log('Player 1 wins this round!');
+        roundResult = 'Humans win this round!';
         playerScore++;
         console.log('The score is... Player: ' + playerScore + ' and Computer: ' + computerScore);
     //error
     } else {
         console.log('An error occured this round.');
     }
+    //Update the human score on the score board
+    let humanScoreNumber = document.getElementById('human-score-number');
+    humanScoreNumber.textContent = playerScore;
 
+    //Update the computer score on the score board
+    let computerScoreNumber = document.getElementById('computer-score-number');
+    computerScoreNumber.textContent = computerScore;
+
+    //Remove prev results (if they exist) and output new results
+    if (compPlayOutput != null) {
+        let needRemoved = document.getElementById('computer-chose');
+         needRemoved.remove(); 
+     }
+    compPlayOutput = document.createElement('p')
+    compPlayOutput.setAttribute('id','computer-chose');
+    compPlayOutput.textContent = compPlayResult;
+    let compChoseDiv = document.getElementById('comp-chose-div');
+    compChoseDiv.appendChild(compPlayOutput);
+    
+    let roundResultsOutput = document.getElementById('round-result')
+    roundResultsOutput.textContent = roundResult;
 };
-// console.log(singleRound(playerChoice, computerPlay() ));
+
+
+
+
+//DOM manipulation
+const rockButton = document.querySelector('#rock');
+rockButton.addEventListener('click', singleRoundRock);
+
+const paperButton = document.querySelector('#paper');
+paperButton.addEventListener('click', singleRoundPaper);
+
+const scissorsButton = document.querySelector('#scissors');
+scissorsButton.addEventListener('click', singleRoundScissors);
+
 
 
 // game function that runs 5 rounds of RPC
@@ -136,12 +232,5 @@ let singleRoundScissors = function(playerSelection, computerSelection) {
 // };
 // game();
 
-
-const rockButton = document.querySelector('#rock');
-rockButton.addEventListener('click', singleRoundRock);
-
-const paperButton = document.querySelector('#paper');
-paperButton.addEventListener('click', singleRoundPaper);
-
-const scissorsButton = document.querySelector('#scissors');
-scissorsButton.addEventListener('click', singleRoundScissors);
+// let resultsDiv = document.getElementById('results');
+//     resultsDiv.insertBefore(compPlayOutput, resultsDiv.lastElementChild);
